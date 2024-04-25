Fix gate matrix computation

quasimp/gates/rz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import math
+import cmath
 import numpy as np
 
 from .gate import Gate
@@ -18,7 +18,7 @@ class RZ(Gate):
 
     def _create_matrix(self) -> np.ndarray:
         base_matrix = np.array(
-            [[math.exp(-1j * self.theta / 2), 0], [0, math.exp(1j * self.theta / 2)]],
+            [[cmath.exp(-1j * self.theta / 2), 0], [0, cmath.exp(1j * self.theta / 2)]],
             dtype=complex,
         )
 