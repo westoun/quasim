c05eb8f48375a6110b2f6fb0e1b1b22b32df7d3e
westoun <christoph.werner.stein@gmail.com>
2024-05-14 15:19:39 +0200

    Enhance benchmarking scope

--- run_benchmarking.py
+++ run_benchmarking.py
@@ -11,3 +11,5 @@ if __name__ == "__main__":
     run_time_benchmark(qubit_num=7)
     run_time_benchmark(qubit_num=8)
     run_result_benchmark(qubit_num=3)
+    run_result_benchmark(qubit_num=5)
+    run_result_benchmark(qubit_num=7)
