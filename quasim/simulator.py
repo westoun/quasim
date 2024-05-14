7a8578f1aee3b225cc742334e06ac75b372b2dc4
westoun <christoph.werner.stein@gmail.com>
2024-05-14 15:09:16 +0200

    Move functions to utils

--- quasim/simulator.py
+++ quasim/simulator.py
@@ -5,71 +5,18 @@ from typing import List, Dict
 
 from .circuit import Circuit
 from .gates import IGate, Swap, Gate, CGate, CCGate
-from .gates.utils import (
-    create_double_controlled_matrix,
-    create_controlled_matrix,
-    create_matrix,
+from .utils import (
+    QubitGroup,
+    is_in_ket0,
+    is_in_ket1,
+    initialize_qubit_groups,
+    get_sorted_state,
+    apply_gate,
+    apply_cgate,
+    apply_ccgate,
+    apply_swap_gate,
+    select_affected_qubit_group,
 )
-from .utils import QubitGroup
-
-QUBIT_STARTING_STATE = np.zeros(2, dtype=np.complex128)
-QUBIT_STARTING_STATE[0] = 1
-
-
-def apply_swap_gate(qubit_groups: List[QubitGroup], gate: Swap) -> None:
-    """Swap the indices of the qubits targetted by the swap gate
-    in place.
-    """
-    for qubit_group in qubit_groups:
-        for i in range(len(qubit_group.qubits)):
-            if qubit_group.qubits[i] == gate.qubit1:
-                qubit_group.qubits[i] = gate.qubit2
-
-            elif qubit_group.qubits[i] == gate.qubit2:
-                qubit_group.qubits[i] = gate.qubit1
-
-
-def initialize_qubit_groups(qubit_num: int) -> List[QubitGroup]:
-    """Create a list of qubit groups where each qubit group contains
-    exactly one qubit id.
-    """
-    qubit_groups: List[QubitGroup] = []
-    for i in range(qubit_num):
-        qubit_groups.append(QubitGroup(qubits=[i], state=QUBIT_STARTING_STATE))
-    return qubit_groups
-
-
-def select_affected_qubit_group(
-    qubit_groups: List[QubitGroup], qubit_id: int
-) -> QubitGroup:
-    """Select the qubit group whose qubit id
-    is affected by the specified gate.
-    """
-
-    for qubit_group in qubit_groups:
-        if qubit_id in qubit_group.qubits:
-            return qubit_group
-
-    raise ValueError(f"No matching qubit group found for qubit '{qubit_id}'")
-
-
-def select_affected_qubit_groups(
-    qubit_groups: List[QubitGroup], qubit_ids: List[int]
-) -> List[QubitGroup]:
-    """Select the subset of qubit groups whose qubit ids
-    are affected by the specified gate.
-    """
-
-    relevant_qubit_groups: List[QubitGroup] = []
-    for qubit_group in qubit_groups:
-        for qubit in qubit_group.qubits:
-            if qubit in qubit_ids:
-                if qubit_group not in relevant_qubit_groups:
-                    relevant_qubit_groups.append(qubit_group)
-
-                break
-
-    return relevant_qubit_groups
 
 
 def merge_qubit_groups(
@@ -93,51 +40,6 @@ def merge_qubit_groups(
     return merged_qubit_group
 
 
-def apply_gate(relevant_qubit_group: QubitGroup, gate: Gate) -> None:
-    """Apply the action of the specified gate onto the selected
-    qubit group in place.
-    """
-    target_qubit = relevant_qubit_group.qubits.index(gate.target_qubit)
-    matrix = create_matrix(
-        gate.matrix,
-        target_qubit=target_qubit,
-        qubit_num=len(relevant_qubit_group.qubits),
-    )
-    relevant_qubit_group.state = np.matmul(matrix, relevant_qubit_group.state)
-
-
-def apply_cgate(relevant_qubit_group: QubitGroup, gate: CGate) -> None:
-    """Apply the action of the specified controlled gate onto the
-    selected qubit group in place.
-    """
-    target_qubit = relevant_qubit_group.qubits.index(gate.target_qubit)
-    control_qubit = relevant_qubit_group.qubits.index(gate.control_qubit)
-    matrix = create_controlled_matrix(
-        gate.matrix,
-        control_qubit=control_qubit,
-        target_qubit=target_qubit,
-        qubit_num=len(relevant_qubit_group.qubits),
-    )
-    relevant_qubit_group.state = np.matmul(matrix, relevant_qubit_group.state)
-
-
-def apply_ccgate(relevant_qubit_group: QubitGroup, gate: CCGate) -> None:
-    """Apply the action of the specified double controlled gate onto the
-    selected qubit group in place.
-    """
-    target_qubit = relevant_qubit_group.qubits.index(gate.target_qubit)
-    control_qubit1 = relevant_qubit_group.qubits.index(gate.control_qubit1)
-    control_qubit2 = relevant_qubit_group.qubits.index(gate.control_qubit2)
-    matrix = create_double_controlled_matrix(
-        gate.matrix,
-        control_qubit1=control_qubit1,
-        control_qubit2=control_qubit2,
-        target_qubit=target_qubit,
-        qubit_num=len(relevant_qubit_group.qubits),
-    )
-    relevant_qubit_group.state = np.matmul(matrix, relevant_qubit_group.state)
-
-
 def aggregate_qubit_groups(qubit_groups: List[QubitGroup]) -> QubitGroup:
     """Combine a list of qubit groups into a new joined qubit group."""
     aggregated_qubit_group = qubit_groups[0]
@@ -147,46 +49,6 @@ def aggregate_qubit_groups(qubit_groups: List[QubitGroup]) -> QubitGroup:
             aggregated_qubit_group.state, qubit_group.state
         )
     return aggregated_qubit_group
-
-
-def get_sorted_state(qubit_group: QubitGroup) -> np.ndarray:
-    """Return a sorted version of the state of a qubit group
-    based on the order of the group's qubit ids.
-    """
-    qubit_num = len(qubit_group.qubits)
-
-    sorting_order = [0] * (2**qubit_num)
-    for i in range(2**qubit_num):
-
-        remainder = i
-        for j in reversed(range(qubit_num)):
-
-            if remainder >= 2**j:
-                sorting_order[i] += 2 ** (
-                    qubit_num - qubit_group.qubits[qubit_num - j - 1] - 1
-                )
-                remainder -= 2**j
-
-    sorted_state = [0] * 2**qubit_num
-    for i in range(2**qubit_num):
-        sorted_state[sorting_order[i]] = qubit_group.state[i]
-
-    sorted_state = np.asarray(sorted_state, dtype=np.complex128)
-    return sorted_state
-
-
-def is_in_ket0(qubit_group: QubitGroup) -> bool:
-    if len(qubit_group.qubits) != 1:
-        raise NotImplementedError()
-
-    return qubit_group.state.tolist()[0] == 1
-
-
-def is_in_ket1(qubit_group: QubitGroup) -> bool:
-    if len(qubit_group.qubits) != 1:
-        raise NotImplementedError()
-
-    return qubit_group.state.tolist()[1] == 1
 
 
 class QuaSim:
--- quasim/utils.py
+++ quasim/utils.py
@@ -6,6 +6,16 @@ import numpy as np
 from typing import List
 import warnings
 
+from .gates import Swap, Gate, CGate, CCGate
+from .gates.utils import (
+    create_double_controlled_matrix,
+    create_controlled_matrix,
+    create_matrix,
+)
+
+QUBIT_STARTING_STATE = np.zeros(2, dtype=np.complex128)
+QUBIT_STARTING_STATE[0] = 1
+
 
 @dataclass
 class QubitGroup:
@@ -19,6 +29,7 @@ class QubitGroup:
     def __hash__(self) -> int:
         qubit_string = ",".join([str(qubit) for qubit in self.qubits])
         return hash(qubit_string)
+
 
 def probabilities_from_state(state: np.ndarray) -> np.ndarray:
     """Returns the probabilities corresponding to a quantum
@@ -66,3 +77,136 @@ def probability_dict_from_state(state: np.ndarray) -> np.ndarray:
         probability_dict[state] = probability
 
     return probability_dict
+
+
+def is_in_ket0(qubit_group: QubitGroup) -> bool:
+    """Indicate if a specified qubit group is in ket0 state.
+    If the qubit group contains more than 1 qubit, a
+    NotImplementedError is raised.
+    """
+
+    if len(qubit_group.qubits) != 1:
+        raise NotImplementedError()
+
+    return qubit_group.state.tolist()[0] == 1
+
+
+def is_in_ket1(qubit_group: QubitGroup) -> bool:
+    """Indicate if a specified qubit group is in ket1 state.
+    If the qubit group contains more than 1 qubit, a
+    NotImplementedError is raised.
+    """
+
+    if len(qubit_group.qubits) != 1:
+        raise NotImplementedError()
+
+    return qubit_group.state.tolist()[1] == 1
+
+
+def initialize_qubit_groups(qubit_num: int) -> List[QubitGroup]:
+    """Create a list of qubit groups where each qubit group contains
+    exactly one qubit id.
+    """
+    qubit_groups: List[QubitGroup] = []
+    for i in range(qubit_num):
+        qubit_groups.append(QubitGroup(qubits=[i], state=QUBIT_STARTING_STATE))
+    return qubit_groups
+
+
+def get_sorted_state(qubit_group: QubitGroup) -> np.ndarray:
+    """Return a sorted version of the state of a qubit group
+    based on the order of the group's qubit ids.
+    """
+    qubit_num = len(qubit_group.qubits)
+
+    sorting_order = [0] * (2**qubit_num)
+    for i in range(2**qubit_num):
+
+        remainder = i
+        for j in reversed(range(qubit_num)):
+
+            if remainder >= 2**j:
+                sorting_order[i] += 2 ** (
+                    qubit_num - qubit_group.qubits[qubit_num - j - 1] - 1
+                )
+                remainder -= 2**j
+
+    sorted_state = [0] * 2**qubit_num
+    for i in range(2**qubit_num):
+        sorted_state[sorting_order[i]] = qubit_group.state[i]
+
+    sorted_state = np.asarray(sorted_state, dtype=np.complex128)
+    return sorted_state
+
+
+def apply_gate(relevant_qubit_group: QubitGroup, gate: Gate) -> None:
+    """Apply the action of the specified gate onto the selected
+    qubit group in place.
+    """
+    target_qubit = relevant_qubit_group.qubits.index(gate.target_qubit)
+    matrix = create_matrix(
+        gate.matrix,
+        target_qubit=target_qubit,
+        qubit_num=len(relevant_qubit_group.qubits),
+    )
+    relevant_qubit_group.state = np.matmul(matrix, relevant_qubit_group.state)
+
+
+def apply_cgate(relevant_qubit_group: QubitGroup, gate: CGate) -> None:
+    """Apply the action of the specified controlled gate onto the
+    selected qubit group in place.
+    """
+    target_qubit = relevant_qubit_group.qubits.index(gate.target_qubit)
+    control_qubit = relevant_qubit_group.qubits.index(gate.control_qubit)
+    matrix = create_controlled_matrix(
+        gate.matrix,
+        control_qubit=control_qubit,
+        target_qubit=target_qubit,
+        qubit_num=len(relevant_qubit_group.qubits),
+    )
+    relevant_qubit_group.state = np.matmul(matrix, relevant_qubit_group.state)
+
+
+def apply_ccgate(relevant_qubit_group: QubitGroup, gate: CCGate) -> None:
+    """Apply the action of the specified double controlled gate onto the
+    selected qubit group in place.
+    """
+    target_qubit = relevant_qubit_group.qubits.index(gate.target_qubit)
+    control_qubit1 = relevant_qubit_group.qubits.index(gate.control_qubit1)
+    control_qubit2 = relevant_qubit_group.qubits.index(gate.control_qubit2)
+    matrix = create_double_controlled_matrix(
+        gate.matrix,
+        control_qubit1=control_qubit1,
+        control_qubit2=control_qubit2,
+        target_qubit=target_qubit,
+        qubit_num=len(relevant_qubit_group.qubits),
+    )
+    relevant_qubit_group.state = np.matmul(matrix, relevant_qubit_group.state)
+
+
+def apply_swap_gate(qubit_groups: List[QubitGroup], gate: Swap) -> None:
+    """Swap the indices of the qubits targetted by the swap gate
+    in place.
+    """
+    for qubit_group in qubit_groups:
+        for i in range(len(qubit_group.qubits)):
+            if qubit_group.qubits[i] == gate.qubit1:
+                qubit_group.qubits[i] = gate.qubit2
+
+            elif qubit_group.qubits[i] == gate.qubit2:
+                qubit_group.qubits[i] = gate.qubit1
+
+
+def select_affected_qubit_group(
+    qubit_groups: List[QubitGroup], qubit_id: int
+) -> QubitGroup:
+    """Select the qubit group whose qubit id
+    is affected by the specified gate. Raise ValueError
+    if no matching qubit group is found.
+    """
+
+    for qubit_group in qubit_groups:
+        if qubit_id in qubit_group.qubits:
+            return qubit_group
+
+    raise ValueError(f"No matching qubit group found for qubit '{qubit_id}'")
